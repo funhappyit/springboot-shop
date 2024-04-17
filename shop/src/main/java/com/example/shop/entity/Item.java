85219b8f2df293fac8ab6bf630c29c459414ae68
한유경 <yugyeong.han@seculayer.com>
2024-04-17 16:37:28 +0900

    2024-04-17 스프링 부트 +thymeleaf+jpa 프로젝트

---
+++ shop/src/main/java/com/example/shop/entity/Item.java
@@ -0,0 +1,52 @@
+package com.example.shop.entity;
+
+import java.time.LocalDateTime;
+
+import com.example.shop.constant.ItemSellStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@ToString
+public class Item {
+
+	@Id
+	@Column(name="item_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id; //상품 코드
+
+	@Column(nullable = false,length = 50)
+	private String itemNm; //상품명
+
+	@Column(name = "price",nullable = false)
+	private int price;//가격
+
+	@Column(nullable = false)
+	private int stockNumber; //재고 수량
+
+	@Lob
+	@Column(nullable = false)
+	private String itemDetail; //상품 상세 설명
+
+	@Enumerated(EnumType.STRING)
+	private ItemSellStatus itemSellStatus; //상품 판매 상태
+
+	private LocalDateTime regTime; //등록 시간
+
+	private LocalDateTime updateTime; //수정 시간
+
+}
